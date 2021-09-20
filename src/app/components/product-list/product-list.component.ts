b5da4289b049690bd7fa39f65af65ca2823de1ea
Andrei Smadici <Andrei.Smadici@yahoo.com>
2021-09-20 17:01:46 +0300

    Define routes

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -12,14 +13,31 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[];
-  constructor(private productService: ProductService) { }
+  currentCategoryId: number;
+  constructor(private productService: ProductService,
+        private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts(){
-    this.productService.getProductList().subscribe(
+
+    // check if "id" param is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    if(hasCategoryId){
+      //get the "id" param string => convert to number with "+"
+        this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
+    else{
+      // not categ id available => id = 1
+      this.currentCategoryId = 1;
+    }
+
+    // get the products for the given category id 
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -16,8 +16,13 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList (): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProductList (theCategoryId: number): Observable<Product[]> {
+      //To Do : need to build URL based on category -- Done
+
+      const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
 
