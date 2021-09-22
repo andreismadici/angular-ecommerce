7168c90cf1174db2d4506028d0e49910f97033b6
Andrei Smadici <Andrei.Smadici@yahoo.com>
2021-09-22 12:06:56 +0300

    Enable Shopping Cart Status -- Observable

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,7 +1,9 @@
 import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -27,6 +29,7 @@ export class ProductListComponent implements OnInit {
 
 
   constructor(private productService: ProductService,
+    private cartService: CartService,
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -122,6 +125,16 @@ export class ProductListComponent implements OnInit {
       this.thePageSize=pageSize;
       this.thePageNumber = 1;
       this.listProducts();
+  }
+
+
+  addToCart(theProduct: Product){
+    
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    const theCartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(theCartItem);
   }
 
 }
