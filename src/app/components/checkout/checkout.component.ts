cbfd58ea5c5906211bc525ae3ab0265a56de9ff2
Andrei Smadici <Andrei.Smadici@yahoo.com>
2021-09-24 09:09:02 +0300

    Add Validation Rules / Display Validation Error Messages

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { ShopFormService } from 'src/app/services/shop-form.service';
@@ -30,9 +30,10 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('',[Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('',[Validators.required, Validators.minLength(2)]),
+        email: new FormControl('', 
+              [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -92,6 +93,10 @@ export class CheckoutComponent implements OnInit {
 
   }
 
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName');}
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName');}
+  get email() { return this.checkoutFormGroup.get('customer.email');}
+
   copyShippingAddressToBillingAddress(event){
     if(event.target.checked){
       this.checkoutFormGroup.controls.billingAddress
@@ -116,6 +121,11 @@ export class CheckoutComponent implements OnInit {
     console.log("The email is: "+this.checkoutFormGroup.get('customer').value.email);
     console.log("The country for shipping is " + this.checkoutFormGroup.get('shippingAddress').value.country);
     console.log("The state for shipping is " + this.checkoutFormGroup.get('shippingAddress').value.state);
+
+    if (this.checkoutFormGroup.invalid){
+      this.checkoutFormGroup.markAllAsTouched();
+    }
+
   }
 
   handleMonthsAndYears(){
