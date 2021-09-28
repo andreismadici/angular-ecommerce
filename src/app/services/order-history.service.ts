54dadf2b8501e7807dac3f2ec5dca4eca3c6cfe5
Andrei Smadici <Andrei.Smadici@yahoo.com>
2021-09-28 09:04:16 +0300

    Sort Orders By Date

--- src/app/services/order-history.service.ts
+++ src/app/services/order-history.service.ts
@@ -9,13 +9,12 @@ import { OrderHistory } from '../common/order-history';
 export class OrderHistoryService {
 
   private orderUrl = `http://localhost:8080/api/orders`
-  //    http://localhost:8080/api/orders/search/findByCustomerEmail?email
   constructor(private httpClient: HttpClient) { }
 
   getOrderHistory(theEmail: string): Observable<GetResponseOrderHistory> {
 
     // need to build URL based on the customer email
-    const orderHistoryUrl = `${this.orderUrl}/search/findByCustomerEmail?email=${theEmail}`;
+    const orderHistoryUrl = `${this.orderUrl}/search/findByCustomerEmailOrderByDateCreatedDesc?email=${theEmail}`;
     return this.httpClient.get<GetResponseOrderHistory>(orderHistoryUrl);
   }
 }
