5d86dcac80ee4742cc7221ef564307ea802c982f
Andrei Smadici <Andrei.Smadici@yahoo.com>
2021-09-28 14:14:08 +0300

    Update  Url for Okta

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa20lod8jCxSMEUA5d7',
         issuer: 'https://dev-25054196.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 
